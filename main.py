017133486148cfd96aea7e55995c40916f07dee0
perlick <37789294+perlick@users.noreply.github.com>
2024-01-31 20:45:27 +0000

    extracting decimeated symbols from fake signal

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import Dict
 import math
 import matplotlib.pyplot as plt
 from scipy.io.wavfile import write, read
+from scipy.signal import butter,filtfilt
 
 QAM_SYMBOLS_ENCODE = {
     0b00: -1 + -1j,
@@ -85,20 +86,54 @@ def signal_to_wav(signal, sample_rate_hz, file_name):
 
 def wav_to_signal(file_name):
     a = read(file_name)
-    return np.array(a[1],dtype=float), a[0]
+    sample_rate_hz = a[0]
+    data = np.array(a[1],dtype=float)
+    data /= np.max(np.abs(data),axis=0)
+    return data, sample_rate_hz
+
+def butter_lowpass_filter(data, cutoff, fs, order=2):
+    """ butter loowpass filter has perfect response 
+    
+    cutoff: # desired cutoff frequency of the filter, Hz
+    fs: sample rate of data
+    order: Polynomial order of the signal. sin can be approximated as quadratic
+    """
+
+    nyq = 0.5*fs
+    normal_cutoff = cutoff / nyq
+    # Get the filter coefficients 
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    y = filtfilt(b, a, data)
+    return y
 
 def signal_to_iq_samples(signal, sample_rate_hz, carrier_freq_hz):
     num_samples = len(signal)
     t = np.arange(num_samples)/sample_rate_hz
+    t = t+0.111111
     rx_lo_i = np.sin(2*np.pi*carrier_freq_hz*t)
     rx_lo_q = np.cos(2*np.pi*carrier_freq_hz*t)
 
     rx_I_sym = signal * rx_lo_i
     rx_Q_sym = signal * rx_lo_q
 
-    return rx_I_sym + rx_Q_sym*1j
+    rx_I_sym = butter_lowpass_filter(rx_I_sym, carrier_freq_hz, sample_rate_hz)
+    rx_Q_sym = butter_lowpass_filter(rx_Q_sym, carrier_freq_hz, sample_rate_hz)
+    
+    out = rx_I_sym + rx_Q_sym*1j
+    save_signal_slice_multi([signal, rx_lo_i, rx_lo_q, rx_I_sym, rx_Q_sym], 1, 150, 44e3, "rx_iq_circuit.jpg")
+    
 
-def iq_samples_to_symbols(signal, carrier_freq, symbol_rate_hz, sample_rate_hz):
+    return out
+
+def iq_samples_to_symbols_simple(signal, symbol_rate_hz, sample_rate_hz):
+    samples_per_symbol = sample_rate_hz / symbol_rate_hz
+    num_symbols = int(len(signal) // samples_per_symbol)
+    out = np.zeros(num_symbols, dtype=np.complex_)
+    for i in range(num_symbols-1):
+        out[i] = signal[int(i*samples_per_symbol + 0.5*samples_per_symbol)]
+    return out
+
+def iq_samples_to_symbols_meuller(signal, symbol_rate_hz, sample_rate_hz):
     mu = 0 # initial estimate of phase of sample
     out = np.zeros(len(signal) + 10, dtype=np.complex_)
     out_rail = np.zeros(len(signal) + 10, dtype=np.complex_) # stores values, each iteration we need the previous 2 values plus current value
@@ -118,36 +153,56 @@ def iq_samples_to_symbols(signal, carrier_freq, symbol_rate_hz, sample_rate_hz):
     out = out[2:i_out] # remove the first two, and anything after i_out (that was never filled out)
     return out
     
-def save_spectrogram(signal, fft_size, Fs):
+def save_spectrogram(signal, fft_size, Fs, file_name):
     num_rows = len(signal) // fft_size
     spectrogram = np.zeros((num_rows, fft_size))
     for i in range(num_rows):
         s = np.hamming(fft_size) * signal[i*fft_size:(i+1)*fft_size]
         spectrogram[i,:] += 10*np.log10(np.abs(np.fft.fftshift(np.fft.fft(s)))**2 + 1e-12)
+    plt.cla()
     plt.imshow(spectrogram, aspect='auto', extent = [Fs/-2, Fs/2, len(signal)/Fs, 0])
     plt.xlabel("Frequency [Hz]")
     plt.ylabel("Time [s]")
     # f = np.arange(Fs/-2, Fs/2, Fs/bins)
     # plt.figure(0)
     # plt.plot(f, S,'.-')
-    plt.savefig("fft.jpg")
+    plt.savefig(file_name)
 
-def save_signal_slice(signal, start_t, num_samples, freq):
+def save_signal_slice(signal, start_t, num_samples, freq, file_name):
     start_i = int(start_t*freq)
     slice = signal[start_i:start_i+num_samples]
+    plt.cla()
     plt.figure(0)
     plt.plot(slice, '.-')
-    plt.savefig("signal.jpg")
+    plt.savefig(file_name)
+
+def save_signal_slice_multi(signals, start_t, num_samples, freq, file_name):
+    start_i = int(start_t*freq)
+    plt.cla()
+    plt.figure(0)
+    for signal in signals:
+        slice = signal[start_i:start_i+num_samples]
+        plt.plot(slice, '.-')
+    plt.plot
+    plt.savefig(file_name)
+
+def plot_constellation(rx_symbols, file_name):
+    plt.cla()
+    plt.figure(2)
+    plt.scatter(rx_symbols.real,rx_symbols.imag)
+    plt.savefig(file_name)
 
 if __name__ == "__main__":
 
-    data = bytes.fromhex('deadbeef')
+    msg = 'deadbeef' # first 32 symbols are for time sync
+    msg += '0123456789abcdeffefebaba'*10 # some random hex for fun
+    data = bytes.fromhex(msg)
     # data = bytes.fromhex('ffff0000')
     print(list(data))
 
     data_symbols = data_to_symbols(data, QAM_SYMBOLS_ENCODE, 2)
 
-    print(data_symbols)
+    # print(data_symbols)
 
     sample_rate_hz = 44e3
     symbol_rate_hz = 10
@@ -155,20 +210,28 @@ if __name__ == "__main__":
 
     signal = symbols_to_signal(data_symbols, carrier_freq_hz, symbol_rate_hz, sample_rate_hz)
 
-    print(len(signal))
+    print("tx_signal_len:" + str(len(signal)))
 
     fft_size = 1024
-    save_spectrogram(signal, fft_size, sample_rate_hz)
+    save_spectrogram(signal, fft_size, sample_rate_hz, "tx_waterfall.jpg")
 
-    save_signal_slice(signal, 0.795, 100, sample_rate_hz)
+    save_signal_slice(signal, 0.795, 100, sample_rate_hz, "tx_signal")
 
-    signal_to_wav(signal, sample_rate_hz, "test.wav")
+    signal_to_wav(signal, sample_rate_hz, "tx.wav")
 
-    rx_singal, rx_sample_rate_hz = wav_to_signal("Record-2024-0130-103658.wav")
+    rx_signal, rx_sample_rate_hz = wav_to_signal("Record-2024-0131-121827.wav")
+    rx_signal = signal
+    rx_sample_rate_hz = sample_rate_hz
+    print("RX_sample_rate: " + str(rx_sample_rate_hz))
+    save_spectrogram(rx_signal, fft_size, rx_sample_rate_hz, "rx_waterfall.jpg")
+    save_signal_slice(rx_signal, 1, 150, rx_sample_rate_hz, "rx_signal.jpg")
 
-    iq_samples = signal_to_iq_samples(signal, sample_rate_hz, carrier_freq_hz)
+    iq_samples = signal_to_iq_samples(rx_signal, rx_sample_rate_hz, carrier_freq_hz)
+    plot_constellation(iq_samples[int(44e3*5):int(44e3*7)], "rx_iq.jpg")
 
-    rx_symbols = iq_samples_to_symbols(iq_samples, carrier_freq_hz, symbol_rate_hz, sample_rate_hz)
+    rx_symbols = iq_samples_to_symbols_simple(iq_samples, symbol_rate_hz, rx_sample_rate_hz)
+    plot_constellation(rx_symbols, "rx_constallation.jpg")
+    # print(rx_symbols)
 
     # print(data_symbols, 50, 16 * 1000)
 
